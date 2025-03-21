76c214b7f6dead6e551cec34e5c3cfb9de739ab8
Simon-grd <simon.godard@ecoles-epsi.net>
2025-03-21 20:27:47 +0100

    Ajouts
    ajout de l'exp et les atouts aléatoires pendant la game.

--- game.py
+++ game.py
@@ -1,17 +1,39 @@
-from character import Character
+from character import Warrior, Mage, Thief
 from dice import Dice
-from ui import Damages
-
+from ui import UI
 
 if __name__ == "__main__":
-    print("\n")
+    print("\n--- RPG Battle Begins! ---\n")
 
-    char_1 = Character("Jamessss", 20, 8, 3, Dice("red", 6))
-    char_2 = Character("Elsaaaa", 20, 8, 3, Dice("red", 6))
+    char_1 = Warrior("James", 20, 8, 3, Dice("red", 6))
+    char_2 = Mage("Elsa", 20, 8, 3, Dice("blue", 6))
 
-    while (char_1.is_alive() and char_2.is_alive()):
-        dmg = char_1.attack()
-        char_2.defend(dmg)
+    print(char_1)
+    print(char_2)
 
-        dmg = char_2.attack()
-        char_1.defend(dmg)
+    round_num = 1
+    while char_1.is_alive() and char_2.is_alive():
+        print(f"\n--- Round {round_num} ---\n")
+        
+        # attaque du perso 1
+        dmg = char_1.attack(char_2)
+        UI.show_damage(char_2, dmg)
+        
+        if not char_2.is_alive():
+            break
+        
+        # attaque du perso 2
+        dmg = char_2.attack(char_1)
+        UI.show_damage(char_1, dmg)
+        
+        round_num += 1
+    
+    # fin du combat
+    if char_1.is_alive():
+        print(f"\n🏆 {char_1.name} wins the battle! 🏆")
+        char_1.gain_experience(50)
+        UI.show_experience(char_1, 50)
+    else:
+        print(f"\n🏆 {char_2.name} wins the battle! 🏆")
+        char_2.gain_experience(50)
+        UI.show_experience(char_2, 50)
--- ui.py
+++ ui.py
@@ -1,5 +1,12 @@
-class Damages:
-    pass
-
 class UI:
-    pass
+    @staticmethod
+    def show_damage(character, damage):
+        print(f"[red]{character.name} takes {damage} damage![/red]")
+
+    @staticmethod
+    def show_heal(character, heal):
+        print(f"[green]{character.name} heals {heal} HP![/green]")
+
+    @staticmethod
+    def show_experience(character, exp):
+        print(f"[blue]{character.name} gains {exp} experience![/blue]")
